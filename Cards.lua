a3ba75c5a384346363625628f6855da55425c100
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-12-29 04:59:15 -0200

    Create Cards.lua

---
+++ Cards.lua
@@ -0,0 +1,36 @@
+local Player = require("Player")
+local Functions = require("Functions")
+local Cards = {}
+
+Cards.move = function(self)
+
+end
+
+Cards.draw = function(self)
+    print(,self.name,)
+    print(self.left,self.text,self.right)
+end
+
+Cards.pb01 = {
+    name = "PB-0.1",
+    left = nil,
+    right = "T",
+    text = "T = 1"
+    effect = function()
+        return t = 1
+    end
+}
+
+Cards.p01 = {
+    name = "P-0.1",
+    left = "T",
+    right = "Q",
+    text = "If T == 1, then return Q = true."
+    effect = function(left)
+        if t == 1 then
+            return q = true
+        end
+    end
+}
+
+return Cards
