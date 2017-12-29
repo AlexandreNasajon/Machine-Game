272ad5166d649ace60c36cfdb7cf0daf5fffd7d2
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-12-29 05:03:21 -0200

    Update Cards.lua

--- Cards.lua
+++ Cards.lua
@@ -15,7 +15,7 @@ Cards.pb01 = {
     name = "PB-0.1",
     left = nil,
     right = "T",
-    text = "T = 1"
+    text = "T = 1",
     effect = function()
         return t = 1
     end
@@ -25,10 +25,22 @@ Cards.p01 = {
     name = "P-0.1",
     left = "T",
     right = "Q",
-    text = "If T = 1, then Q = true."
-    effect = function(left)
-        if t == 1 then
+    text = "If T = 1, then Q = true.",
+    effect = function(self.left)
+        if self.left == 1 then
             return q = true
+        end
+    end
+}
+
+Cards.pf01 = {
+    name = "PF-01",
+    left = "Q",
+    right = "C",
+    text = "If Q = true, then draw a card.",
+    effect = function(player,enemies,self.left)
+        if self.left == true then
+            Functions.draw(player,1)
         end
     end
 }
